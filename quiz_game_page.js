6ddf4fd8ccb3252c9a0822e7b7515b68d1b45ea9
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-04-18 19:41:34 +0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,3 +1,17 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+	player1_score = 0;
+	player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - "
 function send(){
 
     number1 = document.getElementById("number1").value;
@@ -10,4 +24,33 @@ function send(){
     document.getElementById("output").innerHTML=row;
     document.getElementById("number1").value=""
     document.getElementById("number2").value=""
+}
+
+question_turn = "player_1";
+answer_turn = "player_2";
+
+function check(){
+get_answer = document.getElementById("input_check_box").value;
+if(get_answer==actual_answer){
+
+    if(answer_turn == "player_1"){
+
+update_player1_score = player_score +1;
+document.getElementById("player1_score").innerHTML = update_player1_score;
+    }
+    else{
+
+        update_player2_score = player2_score +1;
+        document.getElementById("player2_score").innerHTML = update_player2_score;
+    }
+    if(question_turn == "player_1"){
+
+        question_turn = "player_2";
+        document.getElementById("player_question").innerHTML = "Question Turn -" + player2_name;
+    }
+    else{
+        question_turn = "player_1";
+        document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name;
+    }
+}
 }
