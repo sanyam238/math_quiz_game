31042a644b38c8226be36fd041b72f1db1921032
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-06-11 10:18:48 +0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -36,14 +36,14 @@ if(get_answer==actual_answer){
 
     if(answer_turn == "player_1"){
 
-update_player1_score = player1_score +1;
-document.getElementById("player1_score").innerHTML = update_player1_score;
+player1_score = player1_score +1;
+document.getElementById("player1_score").innerHTML = player1_score;
     }
     else
     {
 
-        update_player2_score = player2_score +1;
-        document.getElementById("player2_score").innerHTML = update_player2_score;
+        player2_score = player2_score +1;
+        document.getElementById("player2_score").innerHTML = player2_score;
     }
     if(question_turn == "player_1"){
 
